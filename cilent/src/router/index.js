4d8522a0f607cbb829d45f4a79cb6e19c35686b9
Chanyanoot <chanyanootu62@nu.ac.th>
2021-09-24 03:27:26 +0700

    first commit

---
+++ cilent/src/router/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+//import HelloWorld from '@/components/HelloWorld'
+
+import UserIndex from '@/components/Index'
+import UserCreate from '@/components/CreateUser'
+import UserEdit from '@/components/EditUser'
+import UserShow from '@/components/ShowUser'
